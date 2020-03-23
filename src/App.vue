a9e5a38e5b5f1e28eac5d915f57dad721e2cacc8
liuqingyi <1152554865@qq.com>
2020-03-23 11:30:54 +0800

    '项目创建完成'

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="app">根组件</div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App"
+};
 </script>
 
 <style lang="less">
